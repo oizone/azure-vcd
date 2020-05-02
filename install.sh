be98ca4fed3e2cf38078cad62b60cda88b4e4210
Onni Rautanen <oizone@oizone.net>
2020-05-02 16:49:24 +0300

    up

--- install.sh
+++ install.sh
@@ -4,19 +4,9 @@ eth0IP=$(ip addr list dev eth0|grep "inet "|cut -d "/" -f 1|cut -d " " -f 6)
 eth1IP=$(ip addr list dev eth1|grep "inet "|cut -d "/" -f 1|cut -d " " -f 6)
 
 curl -o /tmp/vcd.bin -X GET -H "x-ms-date: $(date -u)" "$VCDMEDIAURL?$REPOTOKEN"
-echo "$VCDMEDIAURL?$REPOTOKEN" > /tmp/url
-echo $(VCDMEDIAURL) > /tmp/url1
-echo $(REPOTOKEN) > /tmp/url2
-set > /tmp/setti
-env > /tmp/env
+curl -o /tmp/certificates.ks -X GET -H "x-ms-date: $(date -u)" "$VCDCERTURL?$REPOTOKEN"
 
-echo $0 > /tmp/para
-echo $1 >> /tmp/para
-echo $2 >> /tmp/para
-echo $3 >> /tmp/para
-echo $4 >> /tmp/para
-echo $5 >> /tmp/para
-
+chmod u+x /tmp/vcd.bin
 
 #/tmp/vcd.bin -ip $eth0IP -cons $eth1IP -dbtype postgres -dbhost $DBNAME.postgres.database.azure.com -dbname vcloud -dbuser vcloud@$DBNAME --enable-ceip false -dbpassword $DBPWD -k /tmp/certificate.ks -w $CERTPWD  -unattended
 
