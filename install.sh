81924319cbe3bbd843a2774e2c8f3fdeb158c110
Onni Rautanen <oizone@oizone.net>
2020-05-02 15:59:24 +0300

    validatae

--- install.sh
+++ install.sh
@@ -3,7 +3,10 @@
 eth0IP=$(ip addr list dev eth0|grep "inet "|cut -d "/" -f 1|cut -d " " -f 6)
 eth1IP=$(ip addr list dev eth1|grep "inet "|cut -d "/" -f 1|cut -d " " -f 6)
 
-curl -o /tmp/vcd.bin -X GET -H "x-ms-date: $(date -u)" "$VCDMEDIAURL?$REPOTOKEN"
+curl -o /tmp/vcd.bin -X GET -H "x-ms-date: $(date -u)" "$(VCDMEDIAURL)?$(REPOTOKEN)"
+echo "$(VCDMEDIAURL)?$(REPOTOKEN)" > /tmp/url
+echo $(VCDMEDIAURL) > /tmp/url1
+echo $(REPOTOKEN) > /tmp/url2
 
 
 #/tmp/vcd.bin -ip $eth0IP -cons $eth1IP -dbtype postgres -dbhost $DBNAME.postgres.database.azure.com -dbname vcloud -dbuser vcloud@$DBNAME --enable-ceip false -dbpassword $DBPWD -k /tmp/certificate.ks -w $CERTPWD  -unattended
