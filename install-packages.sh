a608ffe89f32048efe14598b2fa0b415ef1baca3
Onni Rautanen <oizone@oizone.net>
2020-05-02 15:54:04 +0300

    install test

---
+++ install-packages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yum install -y libICE libSM libXdmcp libXext libXi libXt libXtst redhat-lsb postgresql
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+eth0IP=$(ip addr list dev eth0|grep "inet "|cut -d "/" -f 1|cut -d " " -f 6)
+eth1IP=$(ip addr list dev eth1|grep "inet "|cut -d "/" -f 1|cut -d " " -f 6)
+
+curl -o /tmp/vcd.bin -X GET -H "x-ms-date: $(date -u)" "$VCDMEDIAURL?$REPOTOKEN"
+
+
+#/tmp/vcd.bin -ip $eth0IP -cons $eth1IP -dbtype postgres -dbhost $DBNAME.postgres.database.azure.com -dbname vcloud -dbuser vcloud@$DBNAME --enable-ceip false -dbpassword $DBPWD -k /tmp/certificate.ks -w $CERTPWD  -unattended
+
+#/opt/vmware/vcloud-director/bin/cell-management-tool system-setup --user $VCDUSER --full-name "VCD System Administrator" --email $VCDEMAIL --system-name `hostname` --installation-id 2 -password $VCDPWD --unattended
